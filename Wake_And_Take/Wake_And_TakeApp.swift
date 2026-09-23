c4a5e98cf6d0d6e244f2e50b014ac3273d0c73f2
Jian Ting Li <jiantingli@pursuit.org>
2026-09-23 13:20:33 -0400

    Initial Commit

---
+++ Wake_And_Take/Wake_And_TakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wake_And_TakeApp.swift
+//  Wake_And_Take
+//
+//  Created by Jian Ting Li on 9/23/26.
+//
+
+import SwiftUI
+
+@main
+struct Wake_And_TakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
